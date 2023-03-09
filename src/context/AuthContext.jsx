91afa65bc4a82c3e3c01c4ed718bf863267098fe
Cedric Chatellier <cedricchatellier95.com>
2023-03-09 12:58:17 +0100

    login & logout

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -6,54 +6,55 @@ export const AuthContext = createContext();
 function AuthContextWrapper(props) {
   const [user, setUser] = useState(null);
   const [token, setToken] = useState(null);
-  // const [isLoading, setIsLoading] = useState(true)
+  const [isLoading, setIsLoading] = useState(true);
 
   function storeToken(receivedToken) {
     localStorage.setItem("token", receivedToken);
     setToken(receivedToken);
   }
 
-  //   function getToken() {
-  //     return localStorage.getItem("token");
-  //   }
+  function getToken() {
+    return localStorage.getItem("token");
+  }
 
-  //   function removeToken() {
-  //     localStorage.removeItem("token");
-  //   }
+  function removeToken() {
+    localStorage.removeItem("token");
+  }
 
-  //   async function authenticateUser() {
-  //     const currentToken = getToken();
-  //     setToken(currentToken);
-  //     try {
-  //       const response = await myApi.get("/auth/me", {
-  //         headers: {
-  //           Authorization: `Bearer ${currentToken}`,
-  //         },
-  //       });
-  //       if (response.status === 200) {
-  //         setUser(response.data);
-  //         setIsLoading(false);
-  //       } else {
-  //         setUser(null);
-  //         setIsLoading(false);
-  //       }
-  //     } catch (error) {
-  //       console.log(error.response.data);
-  //       setUser(null);
-  //       setIsLoading(false);
-  //     }
-  //   }
+  async function authenticateUser() {
+    const currentToken = getToken();
+    setToken(currentToken);
+    try {
+      const response = await myApi.get("/auth/user", {
+        headers: {
+          Authorization: `Bearer ${currentToken}`,
+        },
+      });
+      if (response.status === 200) {
+        setUser(response.data);
+        setIsLoading(false);
+      } else {
+        setUser(null);
+        setIsLoading(false);
+      }
+    } catch (error) {
+      console.log(error.response.data);
+      setUser(null);
+      setIsLoading(false);
+    }
+  }
 
-  // useEffect(() => {
-  //   authenticateUser()
-  // }, [])
+  useEffect(() => {
+    authenticateUser();
+  }, []);
 
   return (
     <AuthContext.Provider
       value={{
         storeToken,
         user,
-        //   authenticateUser, removeToken
+        authenticateUser,
+        removeToken,
       }}
     >
       {props.children}
